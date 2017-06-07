b655b124b27fc118ce648ca49700d590b78d5c24
noraworld <noraworld@mail.com>
2017-06-07 23:40:54 +0900

    :sparkles: Import convert function and improve demo site

--- wdt-emoji-bundle.js
+++ wdt-emoji-bundle.js
@@ -56,7 +56,7 @@
         "name": "HEAVY BLACK HEART",
         "short_name": "heart",
         "short_names": ["heart"],
-        "unicode": "",
+        "unicode": "2764-fe0f",
         "sort_order": 1
       }, {
         "has_img_apple": true,
@@ -16039,7 +16039,7 @@
     live('click', '.wdt-emoji-list a.wdt-emoji', function (event) {
       var selection = getSelection(wdtEmojiBundle.input);
 
-      replaceText(wdtEmojiBundle.input, selection, this.dataset.wdtEmojiUnicode);
+      replaceText(wdtEmojiBundle.input, selection, convert(this.dataset.wdtEmojiUnicode));
       fire('select', {el: wdtEmojiBundle.input, event: event, emoji: ':' + this.dataset.wdtEmojiShortname + ':'});
 
       var ce = document.createEvent('Event');
@@ -16469,6 +16469,43 @@
       // @todo - [needim] - check browser compatibilities
       el.selectionStart = el.selectionEnd = (textBefore.length + emo.length);
       el.focus();
+    }
+  };
+
+  /**
+   * Refer from EmojiOne source code
+   * https://github.com/emojione/emojione/blob/865516e2f819f598e2417d3ec143916daaf3111d/lib/js/emojione.js#L398-L427
+   *
+   * Many thanks to EmojiOne!
+   */
+  var convert = function(unicode) {
+    if (unicode.indexOf("-") > -1) {
+      var parts = [];
+      var s = unicode.split('-');
+      for (var i = 0; i < s.length; i++) {
+        var part = parseInt(s[i], 16);
+        if (part >= 0x10000 && part <= 0x10FFFF) {
+          var hi = Math.floor((part - 0x10000) / 0x400) + 0xD800;
+          var lo = ((part - 0x10000) % 0x400) + 0xDC00;
+          part = (String.fromCharCode(hi) + String.fromCharCode(lo));
+        }
+        else {
+          part = String.fromCharCode(part);
+        }
+        parts.push(part);
+      }
+      return parts.join('');
+    }
+    else {
+      var s = parseInt(unicode, 16);
+      if (s >= 0x10000 && s <= 0x10FFFF) {
+        var hi = Math.floor((s - 0x10000) / 0x400) + 0xD800;
+        var lo = ((s - 0x10000) % 0x400) + 0xDC00;
+        return (String.fromCharCode(hi) + String.fromCharCode(lo));
+      }
+      else {
+        return String.fromCharCode(s);
+      }
     }
   };
 
